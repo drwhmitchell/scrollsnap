b384ffa1aeacf55dfb51a1d3dc963721e5f518c2
Bill <bill@silvernovus.com>
2022-08-17 11:03:03 -1000

    double survey

--- survey.js
+++ survey.js
@@ -1,64 +1,107 @@
 
 // Sleep Survey Auto-Generation
 
-const survey = [{question: "What is your AGE?", focus:"AGE", backImage: "survey1.png", min: 10, max:100, default:50},
-                {question: "When were you IN-BED/ ASLEEP?", focus:"IN-BED/ASLEEP", backImage: "survey2.png", min: 0, max:12, default:10},
-                {question: "When did you WAKE/GETUP?", focus:"WAKE/GET-UP", backImage: "survey3.png", min: 0, max:12, default:6},
-                {question: "How often were you AWOKEN?", focus:"AWOKEN", backImage: "survey4.png", min: 0, max:10, default:5},
-                {question: "How long did you STAY AWAKE?", focus:"STAY AWAKE", backImage: "survey5.png", min: 0, max:10, default:7},
-                {question: "How much did you DREAM?", focus:"DREAM", backImage: "survey6.png", min: 0, max:10, default:5},
-                {question: "How do your MUSCLES FEEL?", focus:"MUSCLES FEEL", backImage: "survey7.png", min: 0, max:10, default:5},
-                {question: "How positive is your MOOD?", focus:"MOODL", backImage: "survey8.png", min: 0, max:10, default:5},
-                {question: "Assign and OVERALL SLEEP RATING", focus:"OVERALL SLEEP RATING", backImage: "survey9.png", min: 0, max:10, default:5},
+const surveys = [
+                    [
+                        {question: "What is your AGE?", focus:"AGE", backImage: "survey1.png", min: 10, max:100, default:50, answer:50},
+                        {question: "When were you IN-BED/ ASLEEP?", focus:"IN-BED/ ASLEEP", backImage: "survey2.png", min: 0, max:12, default:10, answer:10},
+                        {question: "When did you WAKE/GETUP?", focus:"WAKE/GETUP", backImage: "survey3.png", min: 0, max:12, default:6, answer:6},
+                        {question: "How often were you AWOKEN?", focus:"AWOKEN", backImage: "survey4.png", min: 0, max:10, default:5, answer:5},
+                        {question: "How long did you STAY AWAKE?", focus:"STAY AWAKE", backImage: "survey5.png", min: 0, max:10, default:7, answer:7},
+                        {question: "How much did you DREAM?", focus:"DREAM", backImage: "survey6.png", min: 0, max:10, default:5, answer:5},
+                        {question: "How do your MUSCLES FEEL?", focus:"MUSCLES FEEL", backImage: "survey7.png", min: 0, max:10, default:5, answer:5},
+                        {question: "How positive is your MOOD?", focus:"MOOD", backImage: "survey8.png", min: 0, max:10, default:5, answer:5},
+                        {question: "Assign and OVERALL SLEEP RATING", focus:"OVERALL SLEEP RATING", backImage: "survey9.png", min: 0, max:10, default:5, answer:5},
+                    ],
+                    [
+                        {question: "How OLD are you?", focus:"OLD", backImage: "survey-generic.png", min: 10, max:100, default:50, answer:50},
+                        {question: "Time of IN-BED & ASLEEP?", focus:"IN-BED & ASLEEP", backImage: "survey-generic.png", min: 0, max:12, default:10, answer:10},
+                        {question: "Time of WAKE & GETUP?", focus:"WAKE & GETUP", backImage: "survey-generic.png", min: 0, max:12, default:6, answer:6},
+                        {question: "Frequency of WAKES?", focus:"WAKES", backImage: "survey-generic.png", min: 0, max:10, default:5, answer:5},
+                        {question: "Duration of WAKES?", focus:"WAKES", backImage: "survey-generic.png", min: 0, max:10, default:7, answer:7},
+                        {question: "Amount of DREAMING?", focus:"DREAMING", backImage: "survey-generic.png", min: 0, max:10, default:5, answer:5},
+                        {question: "Level of MUSCLE RELAXATION?", focus:"MUSCLE RELAXATION", backImage: "survey-generic.png", min: 0, max:10, default:5, answer:5},
+                        {question: "Relative MOOD?", focus:"MOOD", backImage: "survey-generic.png", min: 0, max:10, default:5, answer:5},
+                        {question: "Overall SLEEP RATING", focus:"SLEEP RATING", backImage: "survey-generic.png", min: 0, max:10, default:5, answer:5},
+                    ],
                 ];
 
 function initializePage() {
     console.log("Initializing Page");
-    console.log("....");
 
+}
+
+var gSurveyNum = 0;
+
+function RunSurvey(surveyNum) {
+    gSurveyNum = surveyNum;         // Tuck this into a global for now.
+    console.log("Running Survey");
+
+    var introEl = document.getElementById("introduction");
     var quizEl = document.getElementById("sleep-survey");
+
+    // Validate survey choice
+    if (surveyNum >= surveys.length || surveyNum < 0)
+        return;
+
+    // Nuke the intro choice section so we move to the survey
+    introEl.innerHTML = "";
 
     var i = 0;
     const offset = 0;  // Used only for debugging
     var completeSection = "";
     var buf = "";
-    var styledQ;
-    for (i=0; i<survey.length; i++) {
-        newQ = InsertFocus(survey[i].question, survey[i].focus);
-        completeSection = "<section class='section' id='section-" + i + offset + "' style='background-image: url(\"img/" + survey[i].backImage + "\");'><div class='question'>";
-        //        <div class="answer" id="answer-8">NORMAL</div>
-        //           <Input class="slider" type="range" name "" value="5" min="1" max="10" onChange="rangeSlide('answer-9', this.value)" onmousemove="rangeSlide('answer-9', this.value)"></Input>
+    for (i=0; i<surveys[surveyNum].length; i++) {
+        newQ = InsertFocus(surveys[surveyNum][i].question, surveys[surveyNum][i].focus);
+        completeSection = "<section class='section' id='section-" + i + offset + "' style='background-image: url(\"img/" + surveys[surveyNum][i].backImage + "\");'><div class='question'>";
         completeSection += newQ + "</div>";
-        completeSection += '<div class="answer" id="answer-' + i + offset + '">' + survey[i].default + '</div>';
-        completeSection += '<Input class="slider" type="range" name "" value="' + survey[i].default + '" min="' + survey[i].min + '" max="' + survey[i].max + '" onChange="rangeSlide(\'answer-' + i + offset + '\', this.value)" onmousemove="rangeSlide(\'answer-' + i + '\', this.value)"></Input>';
+        completeSection += '<div class="answer" id="answer-' + i + offset + '">' + surveys[surveyNum][i].default + '</div>';
+        completeSection += '<Input class="slider" type="range" name "" value="' + surveys[surveyNum][i].default + '" min="' + surveys[surveyNum][i].min + '" max="' + surveys[surveyNum][i].max + '" onChange="rangeSlide(\'answer-' + i + offset + '\', this.value, ' + i + ')" onMouseMove="rangeSlide(\'answer-' + i + offset + '\', this.value,' +  i + ')"></Input>';
         completeSection += "</section>";  // complete survey question
         buf += completeSection;
     }
-    quizEl.innerHTML += buf;
+    quizEl.innerHTML = buf;
+}
+
+
+function RunAnalysis() {
+    console.log("At Analysis trigger for Survey#", gSurveyNum);
+
+    var buf;
+    var analysisGoEl = document.getElementById("analysis-go");
+    analysisGoEl.innerHTML = "";
 
     var analysisEl = document.getElementById("sleep-analysis");
-    buf = "";
-    buf += ' <section class="section" id="section-10">';
-    buf += '<div class="question"><span id="focus-word">CALCULATING YOUR SLEEP...</span></div>';
+    buf += ' <section class="section">';
+    buf += '<div class="question">Here is your SLEEP ANALYSIS...</span></div>';
     buf += '<br><br>';
-    buf += '<Input class="button" id="analyze-button" name=""></Input>';
     buf += '<img src="img/computing-results.gif">'
     buf += '</section> ';
 
-    analysisEl.innerHTML = buf;
+    analysisGoEl.innerHTML = buf;
+
+    // Now compute the analysis
+    ComputeAnalysis(surveys[gSurveyNum].map(el => (el.value != null) ? el.value : el.default));
+}
+
+function ComputeAnalysis(ansList) {
+    console.log("Computing Results for Survey#" + gSurveyNum + " from " + JSON.stringify(ansList));
 
 }
 
-function rangeSlide(section, value) {
+function rangeSlide(section, value, i) {
     document.getElementById(section).innerHTML = value;
+    surveys[gSurveyNum][i].value = value;
+
 }
 
 // insert "<span id='focus-word'>" and a closing "<span>" around KEY in the SENTENCE
 function InsertFocus(sentence, key) {
-    // Right now does nothing!
      //   "What is your <span id='focus-word'>SEX</span>?</div></section>";
 
-    return(sentence);
+    const newSentence = sentence.replace(key, "<span id='focus-word'>" + key + "</span>");
+    return newSentence;
+
 }
 
 
